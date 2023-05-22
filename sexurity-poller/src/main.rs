a2146726495456a83e0366ff9520e9ca3246ba86
Hackermon <therealhackermon@gmail.com>
2023-05-22 13:20:11 +0000

    feat(poller): implement cli arguments

--- sexurity-poller/src/main.rs
+++ sexurity-poller/src/main.rs
@@ -1,24 +1,65 @@
+#![allow(deprecated)]
+
+use clap::{Parser};
+use chrono::Datelike;
+use chrono;
+
+// use std::{env, ffi::OsString, str::FromStr};
 use graphql_client::GraphQLQuery;
-use sexurity_api::hackerone as hackerone;
+use sexurity_api::hackerone::{self as hackerone, HackerOneClient};
 
+
+#[derive(Default, Debug, Parser)]
+#[clap(author = "hackermon", version, about)]
+struct Arguments {
+    #[arg(short = 'T', long = "session_token", env = "SESSION_TOKEN")]
+    hackerone_session_token: Option<String>,
+
+    #[arg(short = 'R', long = "redis", env = "REDIS_URL")]
+    redis: Option<String>,
+
+    #[arg(short = 'H', long = "handle")]
+    hackerone_handle: String,
+
+    #[arg(default_value = "false", long)]
+    disable_reputation_polling: bool,
+
+    #[arg(default_value = "false", long)]
+    disable_hackactivity_polling: bool,
+    
+    #[arg(default_value = "false", long)]
+    disable_user_report_count_polling: bool,
+}
 fn main() {
-    let session_token = "";
-    let csrf_token = hackerone::get_hackerone_csrf_token(session_token).unwrap();
-    println!("{}", csrf_token);
+    let args = Arguments::parse();
 
+    let session_token = args.hackerone_session_token.clone().unwrap_or("".into());
+    let csrf_token = hackerone::get_hackerone_csrf_token(&session_token).unwrap();
     let client = hackerone::HackerOneClient::new(csrf_token, session_token.to_string());
-    // println!("{}", session_token);
 
+    let good_args = ensure_args(&client, &args);
+    if !good_args {
+        panic!("cannot fetch team. ensure your session token is valid and the team name is valid and your session token is in the team (if its private)")
+    }
+
+    println!("a");
+}
+
+
+fn ensure_args(client: &HackerOneClient, args: &Arguments) -> bool {
+    let now = chrono::Utc::now().date_naive();
+
+    // Verify HackerOne handle
     let variables = hackerone::team_year_thank_query::Variables {
-        selected_handle: String::from("roblox"),
-        year: Some(2023),
+        selected_handle: args.hackerone_handle.clone(),
+        year: Some(now.year().into()),
     };
 
     let query = hackerone::TeamYearThankQuery::build_query(variables);
     let response = client.http.post("https://hackerone.com/graphql").json(&query).send().unwrap();
 
-    // println!("{}", response.text().unwrap());
     let data = response.json::<graphql_client::Response<<hackerone::TeamYearThankQuery as GraphQLQuery>::ResponseData>>().unwrap();
-    let query_response = data.data.unwrap();
-    println!("{:#?}", query_response);
+    let can_fetch_team = data.data.expect("Response data not found").selected_team.is_some();
+
+    return can_fetch_team;
 }
